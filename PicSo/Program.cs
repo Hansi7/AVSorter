4d44052dfa6576352347858721f1ecc02afda187
unknown <hansi-go@163.com>
2015-04-22 23:49:09 +0800

    new form

--- PicSo/Program.cs
+++ PicSo/Program.cs
@@ -15,7 +15,7 @@ namespace PicSo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PicSo.Tester());
+            Application.Run(new PicSo.Form1());
         }
     }
 }
